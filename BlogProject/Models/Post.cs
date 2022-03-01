283906c7d2761981d179d4bb3a0670ff0d86cbb8
Natalia Czapska <czapskanatalia@gmail.com>
2022-03-01 16:03:18 +0100

    Create post model

---
+++ BlogProject/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set;  }
+
+        public Post()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+}

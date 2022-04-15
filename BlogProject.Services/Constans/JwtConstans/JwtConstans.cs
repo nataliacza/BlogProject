87997ba1fa388c8de99aa059692843a6625b57fb
Natalia Czapska <czapskanatalia@gmail.com>
2022-04-15 11:59:49 +0200

    Add Jwt Const

---
+++ BlogProject.Services/Constans/JwtConstans/JwtConstans.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogProject.Services.Constans.JwtConstans;
+
+public static class JwtConstans
+{
+    public const string Subject = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+    public const string UniqueName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+    public const string GivenName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname";
+    public const string SurnName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname";
+    public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+}
